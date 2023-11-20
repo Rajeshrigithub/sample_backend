ab4f2cfc6b2e529ac6a3653a83b776fe5598ab98
Rajeshrigithub <143386423+Rajeshrigithub@users.noreply.github.com>
2023-11-21 00:27:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser');
 const authRoutes = require('./Routes/Auth');
 const adminRoutes = require('./Routes/Admin');
 const movieRoutes = require('./Routes/Movie');
-const imageuploadRoutes = require('./Routes/ImageUploadRoutes');
+const imageuploadRoutes = require('./Routes/imageUploadRoutes');
 
 require('dotenv').config();
 require('./db')
